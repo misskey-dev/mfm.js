453a412efe5132e882462a3b1f0bab6793abba78
marihachi <marihachi0620@gmail.com>
2021-03-22 01:43:52 +0900

    update test

--- test/main.ts
+++ test/main.ts
@@ -2,7 +2,7 @@ import assert from 'assert';
 import { parse, parsePlain } from '../built/index';
 import { createNode } from '../built/util';
 import {
-	TEXT, CENTER, FN, UNI_EMOJI, MENTION, CUSTOM_EMOJI, HASHTAG, N_URL
+	TEXT, CENTER, FN, UNI_EMOJI, MENTION, CUSTOM_EMOJI, HASHTAG, N_URL, BOLD, SMALL, ITALIC, STRIKE
 } from './node';
 
 describe('text', () => {
@@ -12,6 +12,184 @@ describe('text', () => {
 		assert.deepStrictEqual(parse(input), output);
 	});
 });
+
+describe('fn', () => {
+	it('basic', () => {
+		const input = '[tada abc]';
+		const output = [
+			FN('tada', { }, [
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+describe('big', () => {
+	it('basic', () => {
+		const input = '***abc***';
+		const output = [
+			FN('tada', { }, [
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容にはインライン構文を利用できる', () => {
+		const input = '***123**abc**123***';
+		const output = [
+			FN('tada', { }, [
+				TEXT('123'),
+				BOLD([
+					TEXT('abc')
+				]),
+				TEXT('123')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容は改行できる', () => {
+		const input = '***123\n**abc**\n123***';
+		const output = [
+			FN('tada', { }, [
+				TEXT('123\n'),
+				BOLD([
+					TEXT('abc')
+				]),
+				TEXT('\n123')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+describe('bold', () => {
+	it('basic', () => {
+		const input = '**abc**';
+		const output = [
+			BOLD([
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容にはインライン構文を利用できる', () => {
+		const input = '**123~~abc~~123**';
+		const output = [
+			BOLD([
+				TEXT('123'),
+				STRIKE([
+					TEXT('abc')
+				]),
+				TEXT('123')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容は改行できる', () => {
+		const input = '**123\n~~abc~~\n123**';
+		const output = [
+			BOLD([
+				TEXT('123\n'),
+				STRIKE([
+					TEXT('abc')
+				]),
+				TEXT('\n123')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+describe('small', () => {
+	it('basic', () => {
+		const input = '<small>abc</small>';
+		const output = [
+			SMALL([
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容にはインライン構文を利用できる', () => {
+		const input = '<small>abc**123**abc</small>';
+		const output = [
+			SMALL([
+				TEXT('abc'),
+				BOLD([
+					TEXT('123')
+				]),
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容は改行できる', () => {
+		const input = '<small>abc\n**123**\nabc</small>';
+		const output = [
+			SMALL([
+				TEXT('abc\n'),
+				BOLD([
+					TEXT('123')
+				]),
+				TEXT('\nabc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+describe('italic 1', () => {
+	it('basic', () => {
+		const input = '<i>abc</i>';
+		const output = [
+			ITALIC([
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容にはインライン構文を利用できる', () => {
+		const input = '<i>abc**123**abc</i>';
+		const output = [
+			ITALIC([
+				TEXT('abc'),
+				BOLD([
+					TEXT('123')
+				]),
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+	it('内容は改行できる', () => {
+		const input = '<i>abc\n**123**\nabc</i>';
+		const output = [
+			ITALIC([
+				TEXT('abc\n'),
+				BOLD([
+					TEXT('123')
+				]),
+				TEXT('\nabc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+describe('italic 2', () => {
+	it('basic', () => {
+		const input = '*abc*';
+		const output = [
+			ITALIC([
+				TEXT('abc')
+			])
+		];
+		assert.deepStrictEqual(parse(input), output);
+	});
+});
+
+
 describe('custom emoji', () => {
 	it('basic', () => {
 		const input = ':abc:';
