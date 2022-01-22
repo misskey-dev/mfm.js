699ee8e31e04e8525adae5fef5357f5d7544dcf4
marihachi <marihachi0620@gmail.com>
2022-01-22 13:39:38 +0900

    wip: refactor

--- src/internal/parser/services/nodeTree.ts
+++ src/internal/parser/services/nodeTree.ts
@@ -1,27 +1,21 @@
 import { MfmInline, MfmNode, MfmPlainNode, TEXT } from '../../../node';
 
-/**
- * push a node to a node array.
- * in this operation, string node is managed.
-*/
 export function pushNode(node: MfmNode | string, nodes: MfmNode[]): void
 export function pushNode(node: MfmInline | string, nodes: MfmInline[]): void
 export function pushNode(node: MfmPlainNode | string, nodes: MfmPlainNode[]): void
 export function pushNode(node: MfmNode | string, nodes: MfmNode[]): void {
 	if (typeof node == 'string') {
-		// store text node
-		let foundText = false;
+		// store the string as a text node
 		if (nodes.length > 0) {
 			const lastNode = nodes[nodes.length - 1];
 			if (lastNode.type == 'text') {
+				// if the last node is a text node, concatenate to it.
 				lastNode.props.text += node;
-				foundText = true;
+				return;
 			}
 		}
-		if (!foundText) {
-			nodes.push(TEXT(node));
-		}
-	} else {
-		nodes.push(node);
+		nodes.push(TEXT(node));
+		return;
 	}
+	nodes.push(node);
 }
