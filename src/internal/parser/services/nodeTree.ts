ec8cf67bd4cb1219d6e81bcfe82c7b41e4b82e14
marihachi <marihachi0620@gmail.com>
2022-01-22 13:47:26 +0900

    wip: refactor

--- src/internal/parser/services/nodeTree.ts
+++ src/internal/parser/services/nodeTree.ts
@@ -4,18 +4,18 @@ export function pushNode(node: MfmNode | string, nodes: MfmNode[]): void
 export function pushNode(node: MfmInline | string, nodes: MfmInline[]): void
 export function pushNode(node: MfmPlainNode | string, nodes: MfmPlainNode[]): void
 export function pushNode(node: MfmNode | string, nodes: MfmNode[]): void {
-	if (typeof node == 'string') {
-		// store the string as a text node
-		if (nodes.length > 0) {
-			const lastNode = nodes[nodes.length - 1];
-			if (lastNode.type == 'text') {
-				// if the last node is a text node, concatenate to it.
-				lastNode.props.text += node;
-				return;
-			}
-		}
-		nodes.push(TEXT(node));
+	if (typeof node != 'string') {
+		nodes.push(node);
 		return;
 	}
-	nodes.push(node);
+	// if the last node is a text node, concatenate to it
+	if (nodes.length > 0) {
+		const lastNode = nodes[nodes.length - 1];
+		if (lastNode.type == 'text') {
+			lastNode.props.text += node;
+			return;
+		}
+	}
+	// generate a text node
+	nodes.push(TEXT(node));
 }
