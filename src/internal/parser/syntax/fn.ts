23573117311683aff52fc5ed5fef2015dac9de88
marihachi <marihachi0620@gmail.com>
2022-01-23 20:39:26 +0900

    wip: fn

--- src/internal/parser/syntax/fn.ts
+++ src/internal/parser/syntax/fn.ts
@@ -3,6 +3,74 @@ import { MatcherContext } from '../services/matcher';
 import { pushNode } from '../services/nodeTree';
 import { CharCode } from '../services/string';
 import { inlineSyntaxMatcher } from '../services/syntaxMatcher';
+import { spacingMatcher } from '../services/utilMatchers';
+
+function argMatcher(ctx: MatcherContext) {
+	let matched;
+
+	// select 1: key + value
+	matched = /^([a-z0-9_]+)=([a-z0-9_.]+)/i.exec(ctx.input.substr(ctx.pos));
+	if (matched != null) {
+		ctx.pos += matched[0].length;
+		const k = matched[1];
+		const v = matched[2];
+		return ctx.ok({
+			k: k,
+			v: v
+		});
+	}
+
+	// select 2: key
+	matched = /^([a-z0-9_]+)/i.exec(ctx.input.substr(ctx.pos));
+	if (matched != null) {
+		ctx.pos += matched[0].length;
+		const k = matched[1];
+		return ctx.ok<{ k: string, v: string | true }>({
+			k: k,
+			v: true
+		});
+	}
+
+	return ctx.fail();
+}
+
+function argsMatcher(ctx: MatcherContext) {
+	let matched;
+	const args: Record<string, string | true> = {};
+
+	// "."
+	if (ctx.input.charCodeAt(ctx.pos) != CharCode.dot) {
+		return ctx.fail();
+	}
+	ctx.pos++;
+
+	// arg list
+	matched = ctx.consume(argMatcher);
+	if (!matched.ok) {
+		return ctx.fail();
+	}
+	args[matched.result.k] = matched.result.v;
+
+	while (true) {
+		let fallback = ctx.pos;
+		// ","
+		if (ctx.input.charCodeAt(ctx.pos) != CharCode.comma) {
+			break;
+		}
+		ctx.pos++;
+
+		// arg
+		matched = ctx.consume(argMatcher);
+		if (!matched.ok) {
+			ctx.pos = fallback;
+			break;
+		}
+
+		args[matched.result.k] = matched.result.v;
+	}
+
+	return ctx.ok(args);
+}
 
 export function fnMatcher(ctx: MatcherContext) {
 	let matched;
@@ -21,16 +89,14 @@ export function fnMatcher(ctx: MatcherContext) {
 	const name = matched[0];
 	ctx.pos += name.length;
 
-	// params
-	// TODO
-	const params = {};
+	// args
+	matched = ctx.tryConsume(argsMatcher);
+	const params = matched.ok ? matched.result : {};
 
 	// spacing
-	matched = /^[ \u3000\t\u00a0]/.exec(ctx.input.substr(ctx.pos));
-	if (matched == null) {
+	if (!ctx.consume(spacingMatcher).ok) {
 		return ctx.fail();
 	}
-	ctx.pos += matched[0].length;
 
 	// children
 	const children: MfmInline[] = [];
