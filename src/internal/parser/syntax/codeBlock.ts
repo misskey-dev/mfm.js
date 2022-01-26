4b0b91eded5bbb6dcd9ff0517e89b6f2196df70b
marihachi <marihachi0620@gmail.com>
2022-01-27 00:50:25 +0900

    wip: codeBlockMatcher

--- src/internal/parser/syntax/codeBlock.ts
+++ src/internal/parser/syntax/codeBlock.ts
@@ -2,7 +2,50 @@ import { CODE_BLOCK } from '../../../node';
 import { MatcherContext } from '../services/matcher';
 
 export function codeBlockMatcher(ctx: MatcherContext) {
-	// TODO
+	let matched;
 
-	return ctx.ok(CODE_BLOCK('', null));
+	// TODO: check line-head
+
+	// "```"
+	if (!ctx.matchStr('```')) {
+		return ctx.fail();
+	}
+	ctx.pos += 3;
+
+	// lang
+	let lang = '';
+	while (true) {
+		if (ctx.matchRegex(/^\r\n|[\r\n]/) != null) break;
+		if (ctx.eof()) break;
+
+		lang += ctx.input.charAt(ctx.pos);
+		ctx.pos++;
+	}
+
+	// LF
+	matched = ctx.matchRegex(/^\r\n|[\r\n]/);
+	if (matched == null) {
+		return ctx.fail();
+	}
+	ctx.pos += matched.length;
+
+	// TODO: code
+
+	// LF
+	matched = ctx.matchRegex(/^\r\n|[\r\n]/);
+	if (matched == null) {
+		return ctx.fail();
+	}
+	ctx.pos += matched.length;
+
+	// "```"
+	if (!ctx.matchStr('```')) {
+		return ctx.fail();
+	}
+	ctx.pos += 3;
+
+	// TODO: check line-tail
+
+	lang = lang.trim();
+	return ctx.ok(CODE_BLOCK('', (lang.length > 0 ? lang : null)));
 }
