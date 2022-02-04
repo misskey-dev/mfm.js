10617829bb4b489575bbd376d4fef0c94a126b84
marihachi <marihachi0620@gmail.com>
2022-02-04 22:00:47 +0900

    url

--- src/internal/mfm/syntax/url.ts
+++ src/internal/mfm/syntax/url.ts
@@ -2,8 +2,6 @@ import { MfmUrl, N_URL } from '../../../node';
 import { cache, Parser } from '../../services/parser';
 import { CharCode } from '../../services/character';
 
-// TODO: apply new api
-
 // TODO: urlParser 括弧のペア
 
 const schemes: string[] = [
@@ -17,9 +15,8 @@ export const urlParser: Parser<MfmUrl> = cache((ctx) => {
 	// scheme
 	let found = false;
 	for (const sch of schemes) {
-		if (ctx.str(sch + ':')) {
+		if (ctx.str(sch + ':').ok) {
 			found = true;
-			ctx.pos += sch.length + 1;
 			break;
 		}
 	}
@@ -27,13 +24,17 @@ export const urlParser: Parser<MfmUrl> = cache((ctx) => {
 		return ctx.fail();
 	}
 
+	// "//"
+	if (!ctx.str('//').ok) {
+		return ctx.fail();
+	}
+
 	// path
-	const matched = ctx.regex(/^\/\/([.,a-z0-9_/:%#@$&?!~=+-]+)/i);
+	const matched = /^[.,a-z0-9_/:%#@$&?!~=+-]+/i.exec(ctx.input.substr(ctx.pos));
 	if (matched == null) {
 		return ctx.fail();
 	}
-	let path = matched[1];
-	ctx.pos += 2;
+	let path = matched[0];
 
 	// (path) last character must not be "." or ","
 	let length = path.length;
@@ -58,19 +59,17 @@ export const urlParser: Parser<MfmUrl> = cache((ctx) => {
 
 export const urlAltParser: Parser<MfmUrl> = cache((ctx) => {
 	// "<"
-	if (!ctx.char(CharCode.lessThan)) {
+	if (!ctx.char(CharCode.lessThan).ok) {
 		return ctx.fail();
 	}
-	ctx.pos++;
 
 	const urlHead = ctx.pos;
 
 	// scheme
 	let found = false;
 	for (const sch of schemes) {
-		if (ctx.str(sch + ':')) {
+		if (ctx.str(sch + ':').ok) {
 			found = true;
-			ctx.pos += sch.length + 1;
 			break;
 		}
 	}
@@ -80,24 +79,25 @@ export const urlAltParser: Parser<MfmUrl> = cache((ctx) => {
 
 	let c = '';
 	while (true) {
-		if (ctx.char(CharCode.greaterThan)) break;
-		if (ctx.regex(/^[ \u3000\t\u00a0]/) != null) break;
-		if (ctx.regex(/^(\r\n|[\r\n])/) != null || ctx.eof()) break;
-
-		c += ctx.input.charAt(ctx.pos);
-		ctx.pos++;
+		// ">" | spacing
+		if (ctx.match(() => ctx.regex(/^[ \u3000\t>]/))) break;
+		// LF
+		if (ctx.match(() => ctx.regex(/^(\r\n|[\r\n])/))) break;
+		// .
+		const match = ctx.anyChar();
+		if (!match.ok) break;
+		c += match.result;
 	}
-	if (c.length < 1) {
+	if (c.length === 0) {
 		return ctx.fail();
 	}
 
 	const urlTail = ctx.pos;
 
 	// ">"
-	if (!ctx.char(CharCode.greaterThan)) {
+	if (!ctx.char(CharCode.greaterThan).ok) {
 		return ctx.fail();
 	}
-	ctx.pos++;
 
 	const value = ctx.input.substring(urlHead, urlTail);
 
