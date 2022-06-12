373972beef10eb99ff3e3635a32a207854154a2a
marihachi <marihachi0620@gmail.com>
2022-06-12 20:30:13 +0900

    parser, success, failure, str, parser.map

--- src/internal/core/index.ts
+++ src/internal/core/index.ts
@@ -1,4 +1,54 @@
 
-export class Parser {
+type Success = {
+	success: true;
+	value: any;
+	index: number;
+};
 
+type Failure = { success: false };
+
+type Reply = Success | Failure;
+
+export function success(index: number, value: any): Success {
+	return {
+		success: true,
+		value: value,
+		index: index,
+	};
 }
+
+export function failure(): Failure {
+	return { success: false };
+}
+
+type ParserHandler = (input: string, index: number, state: any) => Reply
+
+export class Parser {
+	public handler: ParserHandler;
+
+	constructor(handler: ParserHandler) {
+		this.handler = handler;
+	}
+
+	map(fn: (value: any) => any): Parser {
+		return new Parser((input, index, state) => {
+			const result = this.handler(input, index, state);
+			if (!result.success) {
+				return result;
+			}
+			return success(result.index, fn(result.value));
+		});
+	}
+}
+
+export const str = (value: string): Parser => {
+	return new Parser((input, index, state) => {
+		if ((input.length - index) < value.length) {
+			return failure();
+		}
+		if (input.substr(index, value.length) != value) {
+			return failure();
+		}
+		return success(index + value.length, value);
+	});
+};
--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,5 +1,5 @@
-import { MfmNode, MfmSimpleNode } from '../node';
-import { Parser } from './core';
+import { BOLD, MfmNode, MfmSimpleNode, TEXT } from '../node';
+import * as P from './core';
 
 type FullParserOpts = {
 	fnNameList?: string[];
@@ -7,9 +7,49 @@ type FullParserOpts = {
 };
 
 export function fullParser(input: string, opts: FullParserOpts): MfmNode[] {
-	return [];
+	let reply;
+
+	reply = boldAsta.handler(input, 0, {});
+	if (!reply.success) {
+		throw new Error('parsing error');
+	}
+	return [reply.value];
 }
 
 export function simpleParser(input: string): MfmSimpleNode[] {
 	return [];
 }
+
+// boldAsta
+
+const boldAstaMark = P.str('**');
+
+const boldAsta = new P.Parser((input, index, state) => {
+	let reply;
+
+	reply = boldAstaMark.handler(input, index, state);
+	if (!reply.success) {
+		return P.failure();
+	}
+	index = reply.index;
+	const a = reply.value;
+
+	reply = P.str('abc').handler(input, index, state);
+	if (!reply.success) {
+		return P.failure();
+	}
+	index = reply.index;
+	const b = reply.value;
+	const content = [TEXT('abc')];
+
+	reply = boldAstaMark.handler(input, index, state);
+	if (!reply.success) {
+		return P.failure();
+	}
+	index = reply.index;
+	const c = reply.value;
+
+	return P.success(index, [a, b, c]);
+}).map(result => {
+	return BOLD([TEXT(result[1])]);
+});
