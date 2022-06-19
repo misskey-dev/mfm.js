4a96a62d0258c7c2decce70456f1877bd6f87c5a
marihachi <marihachi0620@gmail.com>
2022-06-19 20:33:27 +0900

    fix codeBlock

--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -105,14 +105,14 @@ const lang = P.createLanguage({
 	codeBlock: r => {
 		const mark = P.str('```');
 		return P.seq([
-			P.lineBegin,
+			P.alt([newLine, P.lineBegin]),
 			mark,
 			P.seq([P.notMatch(newLine), P.any], 1).atLeast(0),
 			newLine,
-			P.seq([P.notMatch(P.seq([newLine, mark, P.lineEnd])), P.any], 1).atLeast(1),
+			P.seq([P.notMatch(P.seq([newLine, mark, P.option(newLine)])), P.any], 1).atLeast(1),
 			newLine,
 			mark,
-			P.lineEnd,
+			P.option(newLine),
 		]).map(result => {
 			const lang = (result[2] as string[]).join('').trim();
 			const code = (result[4] as string[]).join('');
