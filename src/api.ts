0a6004b715bfbcbe58883851b7db031e6a7a83b5
marihachi <marihachi0620@gmail.com>
2022-06-26 19:04:21 +0900

    fix

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { fullParser, simpleParser } from './internal/parser';
+import { fullParser, simpleParser } from './internal';
 import { inspectOne, stringifyNode, stringifyTree } from './internal/util';
 import { MfmNode, MfmSimpleNode } from './node';
 
