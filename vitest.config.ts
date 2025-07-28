7b633e0040b875d4971398fc732263cc3096151d
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-07-28 09:37:03 +0900

    fix coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
     test: {
         include: ['test/**/*.ts'],
-    }
+        coverage: {
+            exclude: [
+                ...configDefaults.coverage.exclude!,
+                'src/cli/**/*',
+                'built/**/*',
+                'test-d/**/*',
+            ],
+        }
+    },
 });
