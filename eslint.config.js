514dcb611ffeb2680400e018d5a60ae13ebf5b8e
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-07-28 09:21:27 +0900

    update deps

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import pluginMisskey from '@misskey-dev/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+//@ts-check
+/** @type {import('eslint').Linter.Config[]}  */
+export default [ // eslint-disable-line import/no-default-export
+	...pluginMisskey.configs['recommended'],
+	{
+		ignores: [
+			'**/node_modules',
+			'src/@types/package.json.d.ts',
+			'built',
+			'vitest.config.ts',
+			'test',
+		],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parserOptions: {
+				parser: tsParser,
+				project: ['./tsconfig.json', './test/tsconfig.json'],
+				sourceType: 'module',
+				tsConfigRootDir: import.meta.dirname,	
+			},
+		},
+	},
+	{
+		files: ['**/*.js', '**/*.cjs'],
+		rules: {
+			'@typescript-eslint/no-var-requires': 'off',
+		},
+	},
+];
